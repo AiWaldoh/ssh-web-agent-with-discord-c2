3f09912ed89de1e8984b67b1392e985b4612991e
Kevin <kevin.roy@owasp.org>
2024-03-27 19:09:50 -0300

    added function call to search web

---
+++ app/api3.py
@@ -0,0 +1,121 @@
+import requests
+import logging
+import json
+from termcolor import colored
+
+
+def elegant_print(*args, **kwargs):
+    for arg in args:
+        if isinstance(arg, str):
+            try:
+                # Attempt to load the string as JSON
+                json_object = json.loads(arg)
+                # If successful, pretty-print the JSON string in green
+                print(colored(json.dumps(json_object, indent=2), "green"))
+            except json.JSONDecodeError:
+                # If it's not valid JSON, just print the string in blue
+                print(colored(arg, "blue"))
+        else:
+            # If the argument is not a string, print it in grey
+            print(colored(str(arg), "grey"))
+
+
+class HttpClient:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.headers = {"Authorization": f"Bearer {self.api_key}"}
+
+    def post(self, url, data):
+        response = requests.post(url, headers=self.headers, json=data)
+        return response.json()
+
+    def get(self, url):
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+
+class ChatAPI:
+    def __init__(
+        self,
+        http_client,
+        model_name,
+        api_url,
+        initial_system_message="You are a helpful assistant.",
+    ):
+        self.http_client = http_client
+        self.model_name = model_name
+        self.url = api_url
+        self.messages = [{"role": "system", "content": initial_system_message}]
+        self.max_history = 30
+        self.temperature = 0.7
+        self.functions = []
+
+    def get_system_message(self):
+        return self.messages[0]["content"] if self.messages else None
+
+    def set_system_message(self, content):
+        self._update_or_insert_system_message(content)
+
+    def get_temperature(self):
+        return self.temperature
+
+    def set_temperature(self, temperature):
+        self.temperature = temperature
+
+    def add_function(self, function):
+        self.functions.append(function)
+
+    def _update_or_insert_system_message(self, content):
+        if self.messages:
+            self.messages[0] = {"role": "system", "content": content}
+        else:
+            self.messages.insert(0, {"role": "system", "content": content})
+
+    def send_message(self, content):
+        self._add_user_message(content)
+        self._trim_history()
+        response = self._send_api_request()
+        return self._process_api_response(response)
+
+    def _add_user_message(self, content):
+        self.messages.append({"role": "user", "content": content})
+
+    def _trim_history(self):
+        user_messages = [msg for msg in self.messages if msg["role"] == "user"]
+        if len(user_messages) > self.max_history:
+            index = len(self.messages) - len(user_messages) + self.max_history
+            self.messages = self.messages[index:]
+
+    def _send_api_request(self):
+        data = {
+            "model": self.model_name,
+            "messages": self.messages,
+            "temperature": self.temperature,
+            "functions": self.functions,
+            "function_call": "auto" if self.functions else None,
+        }
+        return self.http_client.post(self.url, data)
+
+    def _process_api_response(self, response):
+        try:
+            if "function_call" in response["choices"][0]["message"]:
+
+                function_call = response["choices"][0]["message"]["function_call"]
+                function_name = function_call["name"]
+                # self._add_assistant_message(json.dumps(function_call))
+                return function_call
+            else:
+                ai_message_content = response["choices"][0]["message"]["content"]
+                if ai_message_content is not None:
+                    self._add_assistant_message(ai_message_content)
+                return response
+        except KeyError:
+            self._remove_last_user_message()
+            logging.error(f"Error in API response: {response}")
+            return response["error"]["message"]
+
+    def _add_assistant_message(self, content):
+        self.messages.append({"role": "assistant", "content": content})
+
+    def _remove_last_user_message(self):
+        self.messages.pop()
--- app/botv2.py
+++ app/botv2.py
@@ -1,12 +1,17 @@
-from playwright.sync_api import sync_playwright
-from api2 import HttpClient, ChatAPI
+from api3 import HttpClient, ChatAPI
 import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import re
-import playwright
+import urllib.parse
 from playwright.async_api import async_playwright
 import asyncio
+from requests.models import Response
+from curl_cffi import requests
+from newspaper import Article
+from OutputFormatter import OutputFormatter
+import json
+from termcolor import colored
 
 load_dotenv()
 BOT_NAME = os.getenv("BOT_NAME")
@@ -32,9 +37,119 @@ JAVASCRIPT_SCR = """
             """
 
 
+def elegant_print(*args, **kwargs):
+    for arg in args:
+        if isinstance(arg, str):
+            try:
+                # Attempt to load the string as JSON
+                json_object = json.loads(arg)
+                # If successful, pretty-print the JSON string in green
+                print(colored(json.dumps(json_object, indent=2), "green"))
+            except json.JSONDecodeError:
+                # If it's not valid JSON, just print the string in blue
+                print(colored(arg, "blue"))
+        else:
+            # If the argument is not a string, print it in grey
+            print(colored(str(arg), "grey"))
+
+
+def url_encode_string(s):
+    return urllib.parse.quote_plus(s)
+
+
+def fetch_article_content(url):
+    """Fetch the content of the given URL using curl_cffi."""
+    r: Response = requests.get(url, impersonate="chrome101")
+    return r.content
+
+
+def parse_article(html_content):
+    """Parse the article using newspaper3k."""
+    article = Article("")
+    article.set_html(html_content)
+    article.parse()
+    article.nlp()  # This is required to compute the summary
+
+    title = article.title
+    text = article.text
+    summary = article.summary
+
+    return {"title": title, "text": text, "summary": summary}
+
+
+def fetch_google_search_results(query, num_results=20, limit=3):
+    """Fetch Google search results for the given query."""
+    query = url_encode_string(query)
+    r: Response = requests.get(
+        f"https://www.google.com/search?client=firefox-b-d&q={query}&num={num_results}",
+        impersonate="chrome101",
+    )
+    soup = BeautifulSoup(r.content, "html.parser")
+    results = soup.find_all("div", class_="tF2Cxc")
+    # Extract details from the search results
+    search_results = []
+    for result in results:
+        title_element = result.find("h3", class_="LC20lb")
+        link_element = result.find("a", href=True)
+        description_element = result.find("div", class_="VwiC3b")
+
+        title = title_element.text if title_element else None
+        link = link_element["href"] if link_element else None
+        description = description_element.text if description_element else None
+
+        # Filtering out links not starting with http or https
+        if link and (link.startswith("http://") or link.startswith("https://")):
+            search_results.append(SearchResult(title, link, description))
+
+    return search_results[:limit]
+
+
+class SearchResult:
+    def __init__(self, title, url, description):
+        self.title = title
+        self.url = url
+        self.description = description
+        self.article_content = None
+        self.article_summary = None
+        self.article_full_text = None
+
+    def fetch_and_parse_article(self):
+        html_content = fetch_article_content(self.url)
+        parsed_data = parse_article(html_content)
+        self.article_content = parsed_data[
+            "title"
+        ]  # This is just to confirm title from newspaper matches our scraped title
+        self.article_full_text = parsed_data["text"]
+        self.article_summary = parsed_data["summary"]
+
+    def fetch_and_parse_article(self, url):
+        html_content = fetch_article_content(url)
+        parsed_data = parse_article(html_content)
+
+        article_data = {
+            "article_url": url,
+            "article_content": parsed_data["title"],
+            "article_full_text": parsed_data["text"],
+            "article_summary": parsed_data["summary"],
+        }
+
+        return article_data
+
+    def display(self):
+        """Display the search result details."""
+        print(f"Title: {self.title}")
+        print(f"URL: {self.url}")
+        print(f"Description: {self.description}")
+        if self.article_content:
+            print(f"Article Title (from newspaper3k): {self.article_content}")
+            print(f"\nFull Text:\n{self.article_full_text}")
+            print(f"\nSummary:\n{self.article_summary}")
+        print("------")
+
+
 class MessageParser:
     def __init__(self, api_handler):
-        self.api_handler = api_handler
+        self.api_handler: ChatAPIHandler = api_handler
 
     def on_message_received(self, message_html):
         if not self._is_valid_message(message_html):
@@ -44,7 +159,7 @@ class MessageParser:
         message_data = self._parse_message(message_soup)
 
         if message_data["has_mention"]:
-            print(message_data)
+            # print(message_data)
             return self.api_handler.handle_message(message_data)
 
     def _parse_message(self, message_soup):
@@ -104,14 +219,26 @@ class DiscordMonitor:
     def __init__(self, message_parser, api_handler):
         self.browser = None
         self.page = None
-        self.message_parser = message_parser
-        self.api_handler = api_handler
+        self.message_parser: MessageParser = message_parser
+        self.api_handler: ChatAPIHandler = api_handler
 
     async def _send_discord_message(self, message):
         if message:
-            await self.page.wait_for_selector('div[role="textbox"]')
-            await self.page.type('div[role="textbox"]', message)
-            await self.page.keyboard.press("Enter")
+            # Split the message by newline characters
+            lines = message.split("\n")
+
+            # Iterate through each line in the message
+            for i, line in enumerate(lines):
+                # Type the line into the textbox
+                await self.page.type('div[role="textbox"]', line)
+                # If this is not the last line, press Shift+Enter to go to the next line
+                if i < len(lines) - 1:
+                    await self.page.keyboard.down("Shift")
+                    await self.page.keyboard.press("Enter")
+                    await self.page.keyboard.up("Shift")
+                # If this is the last line, just press Enter to send the message
+                else:
+                    await self.page.keyboard.press("Enter")
         else:
             print("Empty message. Skipping sending to Discord.")
 
@@ -155,16 +282,86 @@ class DiscordMonitor:
             message = await asyncio.to_thread(
                 self.message_parser.on_message_received, msg
             )
+            #
+            #
+            #
+            # do I parse function calls here?
+            if contains_search_result(message):
+                print(f"Received search result: {len(message)}")
+                search_result_message = await self.handle_search_result(message)
+                await self._send_discord_message(search_result_message)
+            else:
 
-            if message:
-                print(f"Sending message to Discord: {message}")
-                await self._send_discord_message(message)
+                if message:
+                    print("Sending this message to discord")
+                    elegant_print(message)
+                    await self._send_discord_message(message)
+
+    async def handle_search_result(self, search_result):
+        result_message = ""
+        for item in search_result:
+            result_message += f"URL: <{item.url}>\nDescription: {item.description}\n\n"
+        return result_message
+
+
+def contains_search_result(lst):
+    if not isinstance(lst, list):
+        return False
+    return any(isinstance(item, SearchResult) for item in lst)
 
 
 class ChatAPIHandler:
     def __init__(self, chat_api):
         self.chat_api: ChatAPI = chat_api
-        self.chat_api.set_system_message(SYSTEM_MESSAGE)
+        self.chat_api.set_system_message("you are a helpful assistant")
+        self.initialize_functions()
+
+    def initialize_functions(self):
+        custom_functions = [
+            {
+                "name": "execute_command",
+                "description": "Determine if the message is asking to run a command.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "command": {
+                            "type": "string",
+                            "description": "User input command.",
+                        }
+                    },
+                },
+            },
+            {
+                "name": "search_google",
+                "description": "Search for a query on Google",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "search_query": {
+                            "type": "string",
+                            "description": "The query to search on Google if user specified to search.",
+                        }
+                    },
+                    "required": ["search_query"],
+                },
+            },
+            {
+                "name": "load_website",
+                "description": "Load a website URL and provide a summary or its content.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "website_url": {
+                            "type": "string",
+                            "description": "The URL of the website to load if user specified to check a website.",
+                        }
+                    },
+                    "required": ["website_url"],
+                },
+            },
+        ]
+        for func in custom_functions:
+            self.chat_api.add_function(func)
 
     def handle_message(self, message_data):
         if self._is_admin(message_data["user_id"]):
@@ -173,13 +370,17 @@ class ChatAPIHandler:
             return self.send_message(message_data["message_text"])
 
     def _is_admin(self, user_id):
-        return user_id == ADMIN_USER_ID
+        return user_id == "ADMIN_USER_ID"
 
     def send_message(self, message):
         try:
             self.chat_api.set_temperature(1.0)
             response = self.chat_api.send_message(message)
-            if response and "choices" in response and len(response["choices"]) > 0:
+            # print("")
+            # print(response)
+            if isinstance(response, dict) and "name" in response:
+                return self.execute_function(response)
+            elif response and "choices" in response and len(response["choices"]) > 0:
                 return response["choices"][0]["message"]["content"]
             else:
                 print("Unexpected API response format.")
@@ -188,12 +389,50 @@ class ChatAPIHandler:
             print(f"Error occurred during API call")
             return None
 
+    def execute_function(self, function_call):
+        function_name = function_call["name"]
+        print(f"Executing function: {function_name}")
+        elegant_print(function_call)
+        if function_name == "search_google":
+            print("in search_google. Function call detected")
+            search_query = json.loads(function_call["arguments"])["search_query"]
+            print(f"Searching for: {search_query}")
+            results = fetch_google_search_results(search_query, 10)
+            return results
+        elif function_name == "load_website":
+            print("Loading website")
+            # website_url = json.loads(function_call["arguments"])["website_url"]
+            # searcher = SearchResult("title", "url", "description")
+            # res = searcher.fetch_and_parse_article(website_url)
+            # return res["article_full_text"]
+        elif function_name == "execute_command":
+            print("Executing command")
+            # return function_call
+        else:
+            return None
+
+
+async def run_with_retries(discord_monitor, max_retries=5, interval=10):
+    attempt = 0
+    while attempt < max_retries:
+        try:
+            await discord_monitor.run()
+            break  # Exit the loop if the run is successful
+        except Exception as e:
+            attempt += 1
+            print(f"Attempt {attempt} failed with error: {e}")
+            if attempt < max_retries:
+                print(f"Retrying in {interval} seconds...")
+                await asyncio.sleep(
+                    interval
+                )  # Wait for the specified interval before retrying
+
 
 if __name__ == "__main__":
-
     http_client = HttpClient(OPENROUTER_API_KEY)
     chat_api = ChatAPI(http_client, MODEL_NAME, API_URL)
     api_handler = ChatAPIHandler(chat_api)
     message_parser = MessageParser(api_handler)
     discord_monitor = DiscordMonitor(message_parser, api_handler)
-    asyncio.run(discord_monitor.run())
+
+    asyncio.run(run_with_retries(discord_monitor))
